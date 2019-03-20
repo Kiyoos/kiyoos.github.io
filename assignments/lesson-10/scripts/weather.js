57d390c30e959fb1bd6ffcf6e9f56f27c22c761a
Nina Saunders <kiyoos@buyi.edu>
2019-03-20 07:07:34 -0700

    changed http to https in api addresses

--- assignments/lesson-10/scripts/weather.js
+++ assignments/lesson-10/scripts/weather.js
@@ -1,12 +1,12 @@
 let weatherRequest = new XMLHttpRequest();
-let apiURLstring = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=cc37759cf1ea1a86889a1de5b1352725";
+let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=cc37759cf1ea1a86889a1de5b1352725";
 weatherRequest.open('GET', apiURLstring, true);
 weatherRequest.send();
 weatherRequest.onload = function(){
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
     document.getElementById("cur-temp").innerHTML = weatherData.main.temp;
-    let icon = "http://openweathermap.org/img/w/"+ weatherData.weather[0].icon +".png";
+    let icon = "https://openweathermap.org/img/w/"+ weatherData.weather[0].icon +".png";
     let altText = weatherData.weather[0].description;
     document.getElementById('cur-img').setAttribute('src',icon);
     document.getElementById('cur-img').setAttribute('alt',altText);
