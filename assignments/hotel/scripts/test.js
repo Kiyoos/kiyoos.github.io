9c57e9bfdf54e76f50b72d5c53b79ec4ff9cca64
Nina Saunders <kiyoos@buyi.edu>
2019-04-09 17:46:03 -0700

    Week 14 - fixing JSON

--- assignments/hotel/scripts/test.js
+++ assignments/hotel/scripts/test.js
@@ -1,59 +1,60 @@
-var section=document.getElementById('temples');
-var requestJSON = "../json/test.json";
-var request= JSON.parse(requestJSON);
-request.open('GET', requestJSON, true);
+var templeDiv=document.getElementById('temples');
+var requestURL='https://raw.githubusercontent.com/Kiyoos/ninasaunders.github.io/master/assignments/hotel/json/temples.json';
+var request= new XMLHttpRequest();
+request.open('GET', requestURL);
 request.responseType='json';
 request.send();
 request.onload = function() {
-var townInfo = request.response;
-  showTownInfo(townInfo);
+var templeInfo = request.response;
+  showTempleInfo(templeInfo);
 }
 
-function showTownInfo(jsonObj){
-    var townName=jsonObj['towns'];
-    for (var i = 0; i < townName.length; i++) {
-        var correctName = townName[i].name;
-        if (correctName == "Preston" || correctName == "Fish Haven" || correctName == "Soda Springs"){
-            var myArticle = document.createElement('article');
+function showTempleInfo(jsonObj){
+    var templeName=jsonObj['temple'];
+    for (var i = 0; i < templeName.length; i++) {
             var myContainer = document.createElement('div');
             var myH2 = document.createElement('h2');
-            var myH3 = document.createElement('h3');
-            var myTownInfo = document.createElement('div');
-            var myPara1 = document.createElement('p');
-            var myPara2 = document.createElement('p');
-            var myPara3 = document.createElement('p');
             var myImg = document.createElement('img');
+            var myP1 = document.createElement('p');
+            var myP2 = document.createElement('p');
+            var myP3 = document.createElement('p');
+            var myH3a = document.createElement('h3');
+            var myP4 = document.createElement('p');
+            var myH3b = document.createElement('h3');
+            var my5 = document.createElement('p');
+            var myH3c = document.createElement('h3');
+            var myP6 = document.createElement('p');
+            var myH3d = document.createElement('h3');
+            var myP7 = document.createElement('p');
+            //var mytempleInfo = document.createElement('div');
 
-            myH2.textContent=townName[i].name;
-            myH3.textContent=townName[i].motto;
-            myPara1.textContent='Year Founded: ' + townName[i].yearFounded;
-            myPara2.textContent='Population: ' +townName[i].currentPopulation;
-            myPara3.textContent='Annual Rain Fall: ' +townName[i].averageRainfall+ '"';
+            myH2.textContent=templeName[i].name;
+            myH3a.textContent=templeName[i].address;
+            myP1.textContent='Year Founded: ' + templeName[i].telephone;
+            myP2.textContent='Population: ' +templeName[i].email;
+            myP3.textContent='Annual Rain Fall: ' +templeName[i].services[0]+ '"';
 
-            myContainer.setAttribute("class", "townContainer");
-            myTownInfo.setAttribute("class", "townInfo");    
-            if (correctName == "Preston") {
-                myImg.setAttribute("src", "images/town-preston400.jpg");
-                myImg.setAttribute("alt", "Preston Town Picture")
-            } else if(correctName == "Fish Haven") {
-                    myImg.setAttribute("src", "images/town-fishhaven400.jpg");
-                    myImg.setAttribute("alt", "Fish Haven Town Picture");
-            } else {
-                    myImg.setAttribute("src", "images/town-sodasprings400.jpg");
-                    myImg.setAttribute("alt", "Soday Springs Town Picture")
-            } 
-              
+            myContainer.setAttribute("class", "templeContainer");
+            //mytempleInfo.setAttribute("class", "templeInfo");    
+            myImg.setAttribute("src", "images/town-preston400.jpg");
+            myImg.setAttribute("alt", "Preston Town Picture")
+            myImg.setAttribute("src", "images/town-fishhaven400.jpg");
+            myImg.setAttribute("alt", "Fish Haven Town Picture");
+            myImg.setAttribute("src", "images/town-sodasprings400.jpg");
+            myImg.setAttribute("alt", "Soda Springs Town Picture")
             
-            myContainer.appendChild(myH2);
-            myContainer.appendChild(myH3);
-            myContainer.appendChild(myTownInfo)
-            myTownInfo.appendChild(myPara1);
-            myTownInfo.appendChild(myPara2);
-            myTownInfo.appendChild(myPara3);
+            //myContainer.appendChild(mytempleInfo)
+            myContainer.appendChild(myH3b);
+            myContainer.appendChild(myH3c);
+            myContainer.appendChild(myH3d);
+            myContainer.appendChild(myP4);
+            myContainer.appendChild(myH3a);
+            myContainer.appendChild(myP3);
+            myContainer.appendChild(myP2);
+            myContainer.appendChild(myP1);
             myContainer.appendChild(myImg);
-            myArticle.appendChild(myContainer);
-            section.appendChild(myArticle);
+            myContainer.appendChild(myH2);
+            templeDiv.appendChild(myContainer);
         }
-        continue;
+        //continue;
     }
-}
