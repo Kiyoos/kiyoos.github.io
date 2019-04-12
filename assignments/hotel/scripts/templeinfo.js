85fd75c69c94050ea7d3d31154e6b22663f5b997
Nina Saunders <kiyoos@buyi.edu>
2019-04-11 19:46:03 -0700

    Week 14 - Weather Test

--- assignments/hotel/scripts/templeinfo.js
+++ assignments/hotel/scripts/templeinfo.js
@@ -1,59 +1,189 @@
-var section=document.querySelector('section');
-var requestURL='https://byui-cit230.github.io/weather/data/towndata.json';
-var request= new XMLHttpRequest();
+var templeDiv = document.getElementById('temples');
+var requestURL ='https://raw.githubusercontent.com/Kiyoos/ninasaunders.github.io/master/assignments/hotel/json/temples.json';
+var request = new XMLHttpRequest();
 request.open('GET', requestURL);
-request.responseType='json';
+request.responseType ='json';
 request.send();
 request.onload = function() {
-var townInfo = request.response;
-  showTownInfo(townInfo);
+var templeInfo = request.response;
+  showTempleInfo(templeInfo);
 }
 
-function showTownInfo(jsonObj){
-    var townName=jsonObj['towns'];
-    for (var i = 0; i < townName.length; i++) {
-        var correctName = townName[i].name;
-        if (correctName == "Preston" || correctName == "Fish Haven" || correctName == "Soda Springs"){
-            var myArticle = document.createElement('article');
-            var myContainer = document.createElement('div');
-            var myH2 = document.createElement('h2');
-            var myH3 = document.createElement('h3');
-            var myTownInfo = document.createElement('div');
-            var myPara1 = document.createElement('p');
-            var myPara2 = document.createElement('p');
-            var myPara3 = document.createElement('p');
-            var myImg = document.createElement('img');
+function showTempleInfo(jsonObj){
+    var templeName = jsonObj['temple'];
+    for (var i = 0; i < templeName.length; i++) {
+        var myDiv1 = document.createElement('div');
+        var myDiv2 = document.createElement('div');
+        var myDiv3 = document.createElement('div');
+        var myDiv4 = document.createElement('div');
+        var myDiv5 = document.createElement('div');
+        var myDiv6 = document.createElement('div');
+        var myDiv7 = document.createElement('div');
+        var myDiv8 = document.createElement('div');
+        var myH3 = document.createElement('h3');
+        var myImgW = document.createElement('img');
+        var myPw1 = document.createElement('p');
+        var myPw2 = document.createElement('p');
+        var myImgT = document.createElement('img');
+        var myP1 = document.createElement('p');
+        var myP2 = document.createElement('p');
+        var myP3 = document.createElement('p');
+        var myH4a = document.createElement('h4');
+        var myH4b = document.createElement('h4');
+        var myH4c = document.createElement('h4');
+        var myP4 = document.createElement('p');
+        var myH4d = document.createElement('h4');
 
-            myH2.textContent=townName[i].name;
-            myH3.textContent=townName[i].motto;
-            myPara1.textContent='Year Founded: ' + townName[i].yearFounded;
-            myPara2.textContent='Population: ' +townName[i].currentPopulation;
-            myPara3.textContent='Annual Rain Fall: ' +townName[i].averageRainfall+ '"';
+        myDiv1.setAttribute("class", "border templeinfo");
+        myDiv2.setAttribute("class","nametemp");
+        myDiv6.setAttribute("class", "col2");
+        myH3.textContent = templeName[i].name;
 
-            myContainer.setAttribute("class", "townContainer");
-            myTownInfo.setAttribute("class", "townInfo");    
-            if (correctName == "Preston") {
-                myImg.setAttribute("src", "images/town-preston400.jpg");
-                myImg.setAttribute("alt", "Preston Town Picture")
-            } else if(correctName == "Fish Haven") {
-                    myImg.setAttribute("src", "images/town-fishhaven400.jpg");
-                    myImg.setAttribute("alt", "Fish Haven Town Picture");
+//ADDRESSS CODE
+        var addressUL = document.createElement('ul');
+        var addressList = templeName[i].address;
+        for (var j = 0; j < addressList.length; j++){
+            newLI = document.createElement('li');
+            newLI.textContent = templeName[i].address[j];
+            addressUL.appendChild(newLI);
+        }    
+        addressUL.setAttribute("class","tempaddress");
+
+//CURRENT WEATHER CODE
+        var correctName = templeName[i].name;
+        var apiURLstring = "";
+        if (correctName == "Spokane Washington Temple") {
+            myImgT.setAttribute("src", "images/spokanetempleday500.jpg");
+            myImgT.setAttribute("alt", "Spokane Temple")
+            myPw1.setAttribute("id","spo-temp");
+            myImgW.setAttribute("id","spo-img");
+            apiURLstring = "https://api.openweathermap.org/data/2.5/weather?zip=99206&units=imperial&APPID=cc37759cf1ea1a86889a1de5b1352725";
+        } else if(correctName == "Columbia River Washington Temple") {
+            myImgT.setAttribute("src", "images/columbiarivertemple500.jpg");
+            myImgT.setAttribute("alt", "Columbia River Temple");
+            myPw1.setAttribute("id","colriv-temp");
+            myImgW.setAttribute("id","colriv-img");
+            apiURLstring = "https://api.openweathermap.org/data/2.5/weather?zip=99352&units=imperial&APPID=cc37759cf1ea1a86889a1de5b1352725";
+        } else if(correctName == "Seattle Washington Temple") {
+            myImgT.setAttribute("src", "images/seattletemple500.jpg");
+            myImgT.setAttribute("alt", "Seattle Temple");
+            myPw1.setAttribute("id","sea-temp");
+            myImgW.setAttribute("id","sea-img");
+            apiURLstring = "https://api.openweathermap.org/data/2.5/weather?zip=98007&units=imperial&APPID=cc37759cf1ea1a86889a1de5b1352725";
+        } else {
+            myImgT.setAttribute("src", "images/idahofallstemple500.jpg");
+            myImgT.setAttribute("alt", "Idaho Falls Temple")
+            myPw1.setAttribute("id","if-temp");
+            myImgW.setAttribute("id","if-img");
+            apiURLstring = "https://api.openweathermap.org/data/2.5/weather?zip=83402&units=imperial&APPID=cc37759cf1ea1a86889a1de5b1352725";
+        } 
+                
+        myPw2.textContent = "Current Temp";
+
+        var weatherRequest = new XMLHttpRequest();
+        weatherRequest.open('GET', apiURLstring, true);
+        weatherRequest.send();
+        weatherRequest.onload = function(){
+            var weatherData = JSON.parse(weatherRequest.responseText);
+            console.log(weatherData);
+           
+            var icon = "";
+            var altText = "";
+            if (correctName == "Spokane Washington Temple") {
+                icon = "https://openweathermap.org/img/w/"+ weatherData.weather[0].icon +".png";
+                altText = weatherData.weather[0].description;
+            } else if(correctName == "Columbia River Washington Temple") {
+                icon = "https://openweathermap.org/img/w/"+ weatherData.weather[0].icon +".png";
+                altText = weatherData.weather[0].description;
+            } else if(correctName == "Seattle Washington Temple") {
+                icon = "https://openweathermap.org/img/w/"+ weatherData.weather[0].icon +".png";
+                altText = weatherData.weather[0].description;
             } else {
-                    myImg.setAttribute("src", "images/town-sodasprings400.jpg");
-                    myImg.setAttribute("alt", "Soday Springs Town Picture")
+                icon = "https://openweathermap.org/img/w/"+ weatherData.weather[0].icon +".png";
+                altText = weatherData.weather[0].description;
             } 
-              
-            
-            myContainer.appendChild(myH2);
-            myContainer.appendChild(myH3);
-            myContainer.appendChild(myTownInfo)
-            myTownInfo.appendChild(myPara1);
-            myTownInfo.appendChild(myPara2);
-            myTownInfo.appendChild(myPara3);
-            myContainer.appendChild(myImg);
-            myArticle.appendChild(myContainer);
-            section.appendChild(myArticle);
+
+            document.getElementById("spo-temp").innerHTML = weatherData.main.temp;
+            document.getElementById('spo-img').setAttribute('src',icon);
+            document.getElementById('spo-img').setAttribute('alt',altText);
+            document.getElementById("colriv-temp").innerHTML = weatherData.main.temp;
+            document.getElementById('colriv-img').setAttribute('src',icon);
+            document.getElementById('colriv-img').setAttribute('alt',altText);
+            document.getElementById("sea-temp").innerHTML = weatherData.main.temp;
+            document.getElementById('sea-img').setAttribute('src',icon);
+            document.getElementById('sea-img').setAttribute('alt',altText);
+            document.getElementById("if-temp").innerHTML = weatherData.main.temp;
+            document.getElementById('if-img').setAttribute('src',icon);
+            document.getElementById('if-img').setAttribute('alt',altText);
         }
-        continue;
+
+        myP2.textContent = templeName[i].telephone;
+        myP3.textContent = templeName[i].email;
+
+//FINISHED SERVICES CODE        
+        myH4a.textContent = "Services";
+        var serviceUL = document.createElement('ul');
+        var servicesList = templeName[i].services;
+        for (var j = 0; j < servicesList.length; j++){
+            newLI = document.createElement('li');
+            newLI.textContent = templeName[i].services[j];
+            serviceUL.appendChild(newLI);
+        }    
+
+//FINISHED HISTORY CODE          
+        myH4b.textContent = "History";
+        var historyUL = document.createElement('ul');
+        var historyList = templeName[i].history;
+        for (var j = 0; j < historyList.length; j++){
+            newLI = document.createElement('li');
+            newLI.textContent = templeName[i].history[j];
+            historyUL.appendChild(newLI);
+        }   
+
+//FINISHED SCHEDULE CODE
+        myH4c.textContent = "Schedule";
+        myP4.textContent = templeName[i].ordinanceSchedule;
+
+        var sessionUL = document.createElement('ul');
+        var sessionList = templeName[i].sessionSchedule;
+        for (var j = 0; j < sessionList.length; j++){
+            newLI = document.createElement('li');
+            newLI.textContent = templeName[i].sessionSchedule[j];
+            sessionUL.appendChild(newLI);
+        }   
+
+//FINISHED CLOSURE CODE        
+        myH4d.textContent = "Closures";
+        var closureUL = document.createElement('ul');
+        var closureList = templeName[i].templeClosure;
+        for (var j = 0; j < closureList.length; j++){
+            newLI = document.createElement('li');
+            newLI.textContent = templeName[i].templeClosure[j];
+            closureUL.appendChild(newLI);
+        }   
+
+        myDiv1.appendChild(myDiv2);
+        myDiv2.appendChild(myH3);
+        myDiv2.appendChild(myDiv3);
+        myDiv3.appendChild(myImgW);
+        myDiv3.appendChild(myPw1);
+        myDiv3.appendChild(myPw2);
+        myDiv1.appendChild(myImgT);
+        myDiv1.appendChild(addressUL);
+        myDiv1.appendChild(myP2);
+        myDiv1.appendChild(myP3);
+        myDiv6.appendChild(myDiv7);
+        myDiv6.appendChild(myDiv8);
+        myDiv1.appendChild(myDiv6);
+        myDiv7.appendChild(myH4a);
+        myDiv7.appendChild(serviceUL);
+        myDiv8.appendChild(myH4b);
+        myDiv8.appendChild(historyUL);
+        myDiv1.appendChild(myH4c);
+        myDiv1.appendChild(myP4);
+        myDiv1.appendChild(sessionUL);
+        myDiv1.appendChild(myH4d);
+        myDiv1.appendChild(closureUL);
+        templeDiv.appendChild(myDiv1);
     }
 }
--- assignments/hotel/scripts/test.js
+++ assignments/hotel/scripts/test.js
@@ -1,4 +1,4 @@
-var templeDiv=document.getElementById('temples');
+var testUL=document.getElementById('temples');
 var requestURL='https://raw.githubusercontent.com/Kiyoos/ninasaunders.github.io/master/assignments/hotel/json/temples.json';
 var request= new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -12,108 +12,13 @@ var templeInfo = request.response;
 function showTempleInfo(jsonObj){
     var templeName=jsonObj['temple'];
     for (var i = 0; i < templeName.length; i++) {
-            var myContainer = document.createElement('div');
-            var myH2 = document.createElement('h2');
-            var myImg = document.createElement('img');
-            var myP1 = document.createElement('p');
-            var myP2 = document.createElement('p');
-            var myP3 = document.createElement('p');
-            var myH3a = document.createElement('h3');
-            //var myUl = document.createElement('ul');
-            //var myLi = document.createElement('li');
-            var myP4 = document.createElement('p');
-            var myH3b = document.createElement('h3');
-            var myP5 = document.createElement('p');
-            var myH3c = document.createElement('h3');
-            var myP6a = document.createElement('p');
-            var myP6b = document.createElement('p');
-            var myP6c = document.createElement('p');
-            var myH3d = document.createElement('h3');
-            var myP7 = document.createElement('p');
-            //var mytempleInfo = document.createElement('div');
-
-            myH2.textContent = templeName[i].name;
-
-            var street = templeName[i].address;
-            var fullStreet = "";
-            for (var j = 0; j < street.length; j++){
-                fullStreet += templeName[i].address[j]+", ";
-            }
-            myP1.textContent = fullStreet;
-
-            myP2.textContent = templeName[i].telephone;
-            myP3.textContent = templeName[i].email;
-            
-            myH3a.textContent = "Services";
-            var servList = templeName[i].services;
-            var fullServices = "";
-            //var liServices = document.createElement('li');
-            for (var j = 0; j < servList.length; j++){
-                //var liServices = document.createElement('li');
-                //liServices += templeName[i].services[j];
-                fullServices += templeName[i].services[j]+ ", ";
-            }
-            //myLi.textContent = liServices;
-            //myLi.textContent = fullServices;
-            myP4.textContent = fullServices;
-
-            myH3b.textContent = "History";
-            var histList = templeName[i].history;
-            var fullHistory = "";
-            for (var j = 0; j < histList.length; j++){
-                fullHistory += templeName[i].history[j]+", ";
-            }
-            myP5.textContent = fullHistory;
-
-            myH3c.textContent = "Schedule";
-//NEED TO FIGURE OUT THE DAYS IN THIS ONE
-
-            var schedList = templeName[i].sessionSchedule;
-            var fullSchedule = "";
-            for (var j = 0; j < schedList.length; j++){
-                fullSchedule += templeName[i].sessionSchedule[j];
-            }
-            myP6b.textContent = fullSchedule;
-
-            myP6a.textContent = templeName[i].ordinanceSchedule;
-           // myP6b.textContent = "Sundays & Mondays: "+templeName[i].sessionSchedule[0].sunday;
-            //myP6c.textContent = templeName[i].sessionSchedule[2].tuesday[0];
-            
-            myH3d.textContent = "Closures";
-
-//NEET TO FIGURE OUT THE CLOSURE LIST ON THIS ONE
-/*
-            var closeList = templeName[i].templeClosure;
-            var fullClosure = "";
-            for (var j = 0; j < closeList.length; j++){
-                fullClosure += templeName[i].templeClosure[j].year2019[j];
-            }
-            myP7.textContent = fullClosure;
-*/
-            myContainer.setAttribute("class", "templeContainer");
-            //mytempleInfo.setAttribute("class", "templeInfo");    
-            myImg.setAttribute("src", "images/phone.jpg");
-            myImg.setAttribute("alt", "Test Picture")
-            
-            //myContainer.appendChild(mytempleInfo)
-            myContainer.appendChild(myH2);
-            myContainer.appendChild(myImg);
-            myContainer.appendChild(myP1);
-            myContainer.appendChild(myP2);
-            myContainer.appendChild(myP3);
-            myContainer.appendChild(myH3a);
-            //myContainer.appendChild(myUl);
-            //myUl.appendChild(myLi);
-            myContainer.appendChild(myP4);
-            myContainer.appendChild(myH3b);
-            myContainer.appendChild(myP5);
-            myContainer.appendChild(myH3c);
-            myContainer.appendChild(myP6a);
-            myContainer.appendChild(myP6b);
-            myContainer.appendChild(myP6c);
-            myContainer.appendChild(myH3d);
-            myContainer.appendChild(myP7);
-            templeDiv.appendChild(myContainer);
+            var containerUL = document.createElement('ul');
+            var servicesList = templeName[i].services;
+            for (var j = 0; j < servicesList.length; j++){
+                newLI = document.createElement('li');
+                newLI.textContent = templeName[i].services[j];
+                containerUL.appendChild(newLI);
+            }    
+            testUL.appendChild(containerUL);
         }
-        //continue;
     }
